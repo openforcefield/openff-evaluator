faae7ee13f0cabc7a8ad0051a835d7651e02e64f
SimonBoothroyd <simon.boothroyd@choderalab.org>
2019-05-29 06:03:05 +0200

    Added basic support for loading plugins via entry points.

--- propertyestimator/__init__.py
+++ propertyestimator/__init__.py
@@ -7,11 +7,15 @@ Property calculation toolkit from the Open Forcefield Consortium.
 # Safe to remove with Python 3-only code
 from __future__ import absolute_import
 
-# Add imports here
-# from .propertyestimator import *
+import pkg_resources
+
+from ._version import get_versions
+
+# Load in any found plugins.
+for entry_point in pkg_resources.iter_entry_points('propertyestimator.plugins'):
+    entry_point.load()
 
 # Handle versioneer
-from ._version import get_versions
 versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
