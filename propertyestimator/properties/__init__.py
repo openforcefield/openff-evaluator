27ee09e6205857b5596551ff497b92b2d1c5a202
SimonBoothroyd <simon.boothroyd@choderalab.org>
2019-06-21 14:08:40 -0400

    Fixed incorrect properties import order.

--- propertyestimator/properties/__init__.py
+++ propertyestimator/properties/__init__.py
@@ -1,6 +1,6 @@
+from .properties import PropertyPhase, PhysicalProperty, MeasurementSource, CalculationSource, \
+    ParameterGradientKey, ParameterGradient
 from .binding import HostGuestBindingAffinity
 from .density import Density
 from .dielectric import DielectricConstant
 from .enthalpy import EnthalpyOfMixing
-from .properties import PropertyPhase, PhysicalProperty, MeasurementSource, CalculationSource, \
-    ParameterGradientKey, ParameterGradient
