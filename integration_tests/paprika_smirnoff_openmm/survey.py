52664ec0d407f619e5f2efc205ec505ed0f720be
David Slochower <slochower@gmail.com>
2019-09-21 15:25:18 -0700

    Improve logging of results

--- integration_tests/paprika_smirnoff_openmm/survey.py
+++ integration_tests/paprika_smirnoff_openmm/survey.py
@@ -65,15 +65,13 @@ __BODY__ = """
         # Create the protocol which will run the attach pull calculations
         host_guest_protocol = OpenMMPaprikaProtocol(f'host_guest-{orientation}')
 
-        host_guest_protocol.setup = False
-
         host_guest_protocol.substance = substance
         host_guest_protocol.taproom_guest_orientation = orientation
 
         # Set up the required directories.
         host_guest_directory = f'{host}-{guest}-{orientation}'
         os.makedirs(host_guest_directory, exist_ok=True)
-
+        
         host_guest_protocol.thermodynamic_state = thermodynamic_state
 
         host_guest_protocol.number_of_equilibration_steps = 5000
@@ -92,7 +90,15 @@ __BODY__ = """
         host_guest_protocol.force_field = OpenMMPaprikaProtocol.ForceField.SMIRNOFF
         host_guest_protocol.force_field_path = force_field_path
 
+        host_guest_protocol.setup = False
+        host_guest_protocol.simulate = False
+        host_guest_protocol.analyze = True
+
+        if os.path.exists("results.csv"):
+            break
+
         result = host_guest_protocol.execute(host_guest_directory, resources)
+        
 
         if isinstance(result, PropertyEstimatorException):
             logging.info(f'The attach / pull calculations failed with error: {result.message}')
@@ -107,8 +113,8 @@ __BODY__ = """
         sum_protocol = AddBindingFreeEnergies("add_binding_free_energies")
 
         for result in substance_results:
-            logging.info(f"Attach = {result.attach_free_energy.value.to(unit.kilocalorie / unit.mole)} ± {result.attach_free_energy.uncertainty.to(unit.kilocalorie / unit.mole)}",
-                         f"Pull = {result.pull_free_energy.value.to(unit.kilocalorie / unit.mole)} ± {result.pull_free_energy.uncertainty.to(unit.kilocalorie / unit.mole)}")
+            logging.info(f"Attach = {result.attach_free_energy.value.to(unit.kilocalorie / unit.mole)} ± {result.attach_free_energy.uncertainty.to(unit.kilocalorie / unit.mole)}")
+            logging.info(f"Pull = {result.pull_free_energy.value.to(unit.kilocalorie / unit.mole)} ± {result.pull_free_energy.uncertainty.to(unit.kilocalorie / unit.mole)}")
 
         free_energies = [result.attach_free_energy + result.pull_free_energy for result in substance_results]
         logging.info(f"Attach + Pull (0) = {free_energies[0].value.to(unit.kilocalorie / unit.mole)} ± {free_energies[0].uncertainty.to(unit.kilocalorie / unit.mole)}")
@@ -125,8 +131,14 @@ __BODY__ = """
 
     logging.info(f"Attach + Pull (Combined) = {sum_protocol.result} ")
     logging.info(f'Reference = {host_guest_protocol.reference_free_energy}')
-    logging.info(f'ΔG° (without conformational release) = {sum_protocol.results - host_guest_protocol.reference_free_energy}')
-    logging.info(f'Ballpark ΔG° = {sum_protocol.results - host_guest_protocol.reference_free_energy - 8}')                      
+    logging.info(f'ΔG° (without conformational release) = {sum_protocol.result.value - host_guest_protocol.reference_free_energy}')
+    logging.info(f'Ballpark ΔG° = {sum_protocol.result.value - host_guest_protocol.reference_free_energy - 8}')                      
+
+    with open("results.dat", "w") as f:
+        for result in substance_results:
+            f.write(f"{result.host}-{result.guest}-{result.orientation} {result.attach_free_energy} {result.pull_free_energy}"
+        f.write(sum_protocol.result)
+        f.write(host_guest_protocol.reference_free_energy)
 """
 
 __HOST_ONLY_BODY__ = """
@@ -142,8 +154,6 @@ __HOST_ONLY_BODY__ = """
     # Create the protocol which will run the release calculations
     host_protocol = OpenMMPaprikaProtocol('host')
     
-    host_protocol.setup = False
-
     host_protocol.substance = host_substance
     host_protocol.thermodynamic_state = thermodynamic_state
 
@@ -162,6 +172,13 @@ __HOST_ONLY_BODY__ = """
     host_protocol.force_field = OpenMMPaprikaProtocol.ForceField.SMIRNOFF
     host_protocol.force_field_path = force_field_path
 
+    host_protocol.setup = False
+    host_protocol.simulate = False
+    host_protocol.analyze = True
+
+    if os.path.exists("results.csv"):
+        break
+
     result = host_protocol.execute(host_directory, resources)
 
     if isinstance(result, PropertyEstimatorException):
@@ -169,8 +186,9 @@ __HOST_ONLY_BODY__ = """
         return
         
     logging.info(f"Release={host_protocol.release_free_energy} ")
+    with open("results.dat", "w") as f:
+        f.write(host_protocol.release_free_energy)
 
-        
 """
 
 
