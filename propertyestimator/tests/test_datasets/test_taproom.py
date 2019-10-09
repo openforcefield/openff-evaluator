98eba04001e6c5053373cd8c2819fa9f3ec5d001
SimonBoothroyd <simon.boothroyd@choderalab.org>
2019-10-09 15:50:17 -0400

    Added filtering tests.

--- propertyestimator/tests/test_datasets/test_taproom.py
+++ propertyestimator/tests/test_datasets/test_taproom.py
@@ -8,8 +8,19 @@ from propertyestimator.datasets.taproom import TaproomDataSet
 def test_initialization():
     """Test the initialization of a TaproomDataSet."""
 
-    # Mole fraction
     data_set = TaproomDataSet()
 
     assert data_set is not None
     assert len(data_set.properties) > 0
+
+
+def test_filtering():
+    """Test filtering the data set"""
+
+    data_set = TaproomDataSet()
+
+    data_set.filter_by_host_identifiers('bcd', 'acd')
+    data_set.filter_by_guest_identifiers('cbu')
+
+    assert data_set is not None
+    assert len(data_set.properties) == 2
