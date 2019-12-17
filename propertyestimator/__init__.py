6fc075d89575bff96fa40e23e1d3d7bc7fd61606
SimonBoothroyd <simon.boothroyd@colorado.edu>
2019-12-17 16:29:41 -0700

    Clean-up root init.

--- propertyestimator/__init__.py
+++ propertyestimator/__init__.py
@@ -3,10 +3,6 @@ propertyestimator
 Property calculation toolkit from the Open Forcefield Consortium.
 """
 
-# Make Python 2 and 3 imports work the same
-# Safe to remove with Python 3-only code
-from __future__ import absolute_import
-
 import pint
 import pkg_resources
 
