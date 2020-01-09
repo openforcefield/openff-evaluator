9bdcd37de5358dc586b2cc706e51a5a8b7b2bca8
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-01-09 17:15:02 +0000

    Remove redundant property tests. Fix hydration free energy test. Remove redundant server setup code.

--- propertyestimator/tests/utils.py
+++ propertyestimator/tests/utils.py
@@ -1,15 +1,7 @@
 import os
-import shutil
 import uuid
-from enum import Enum
 
-from propertyestimator import server, unit
-from propertyestimator.backends import (
-    ComputeResources,
-    DaskLocalCluster,
-    DaskLSFBackend,
-    QueueWorkerResources,
-)
+from propertyestimator import unit
 from propertyestimator.datasets import (
     CalculationSource,
     PhysicalPropertyDataSet,
@@ -17,108 +9,10 @@ from propertyestimator.datasets import (
 )
 from propertyestimator.forcefield import SmirnoffForceFieldSource
 from propertyestimator.properties import Density, DielectricConstant, EnthalpyOfMixing
-from propertyestimator.storage import LocalFileStorage
 from propertyestimator.storage.data import StoredSimulationData
 from propertyestimator.substances import Component, MoleFraction, Substance
 from propertyestimator.thermodynamics import ThermodynamicState
 from propertyestimator.utils import get_data_filename
-
-
-class BackendType(Enum):
-    LocalCPU = "LocalCPU"
-    LocalGPU = "LocalGPU"
-    GPU = "GPU"
-    CPU = "CPU"
-
-
-def setup_server(
-    backend_type=BackendType.LocalCPU,
-    max_number_of_workers=1,
-    conda_environment="propertyestimator",
-    worker_memory=8 * unit.gigabyte,
-    port=8000,
-):
-
-    working_directory = "working_directory"
-    storage_directory = "storage_directory"
-
-    # Remove any existing data.
-    if os.path.isdir(working_directory):
-        shutil.rmtree(working_directory)
-
-    calculation_backend = None
-
-    if backend_type == BackendType.LocalCPU:
-        calculation_backend = DaskLocalCluster(number_of_workers=1)
-
-    elif backend_type == BackendType.LocalGPU:
-
-        calculation_backend = DaskLocalCluster(
-            number_of_workers=1,
-            resources_per_worker=ComputeResources(
-                1, 1, ComputeResources.GPUToolkit.CUDA
-            ),
-        )
-
-    elif backend_type == BackendType.GPU:
-
-        queue_resources = QueueWorkerResources(
-            number_of_threads=1,
-            number_of_gpus=1,
-            preferred_gpu_toolkit=QueueWorkerResources.GPUToolkit.CUDA,
-            per_thread_memory_limit=worker_memory,
-            wallclock_time_limit="05:59",
-        )
-
-        worker_script_commands = [
-            f'export OE_LICENSE="/home/boothros/oe_license.txt"',
-            f". /home/boothros/miniconda3/etc/profile.d/conda.sh",
-            f"conda activate {conda_environment}",
-            f"module load cuda/9.2",
-        ]
-
-        extra_script_options = ['-m "ls-gpu lt-gpu"']
-
-        calculation_backend = DaskLSFBackend(
-            minimum_number_of_workers=1,
-            maximum_number_of_workers=max_number_of_workers,
-            resources_per_worker=queue_resources,
-            queue_name="gpuqueue",
-            setup_script_commands=worker_script_commands,
-            extra_script_options=extra_script_options,
-            adaptive_interval="1000ms",
-        )
-    elif backend_type == BackendType.CPU:
-
-        queue_resources = QueueWorkerResources(
-            number_of_threads=1,
-            per_thread_memory_limit=worker_memory,
-            wallclock_time_limit="01:30",
-        )
-
-        worker_script_commands = [
-            f'export OE_LICENSE="/home/boothros/oe_license.txt"',
-            f". /home/boothros/miniconda3/etc/profile.d/conda.sh",
-            f"conda activate {conda_environment}",
-        ]
-
-        calculation_backend = DaskLSFBackend(
-            minimum_number_of_workers=1,
-            maximum_number_of_workers=max_number_of_workers,
-            resources_per_worker=queue_resources,
-            queue_name="cpuqueue",
-            setup_script_commands=worker_script_commands,
-            adaptive_interval="1000ms",
-        )
-
-    storage_backend = LocalFileStorage(storage_directory)
-
-    server.EvaluatorServer(
-        calculation_backend=calculation_backend,
-        storage_backend=storage_backend,
-        port=port,
-        working_directory=working_directory,
-    )
 
 
 def create_dummy_substance(number_of_components, elements=None):
