5839ce558a6ec307207a74240e515633e76e9c31
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-03 15:35:31 -0700

    Use path suffix.

--- devtools/scripts/decrypt_test.sh
+++ devtools/scripts/decrypt_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo $TEST_FILE
+echo $TEST_PATH
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $TEST_FILE test.txt.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $TEST_PATH test.txt.gpg
