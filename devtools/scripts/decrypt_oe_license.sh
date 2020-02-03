dc9551d9c00a9660aa3f8b68283b46578bb66e9e
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-03 12:05:42 -0700

    [WIP] Fix decrypt

--- devtools/scripts/decrypt_oe_license.sh
+++ devtools/scripts/decrypt_oe_license.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME
-
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$OE_LICENSE_PASSPHRASE" \
---output $OE_LICENSE oe_license.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$OE_LICENSE_PASSPHRASE" --output $OE_LICENSE oe_license.txt.gpg
