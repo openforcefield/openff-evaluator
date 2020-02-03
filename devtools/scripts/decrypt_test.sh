7ddaded1ab858592fc47a17ba73f11fbf6f67e65
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-03 15:39:45 -0700

    Use dot path.

--- devtools/scripts/decrypt_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo $TEST_PATH
-
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $TEST_PATH test.txt.gpg
