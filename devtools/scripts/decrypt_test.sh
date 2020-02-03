35f13636304a32f06b95bb6fcef71c3240eeb708
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-03 15:30:56 -0700

    Make path relative.

--- devtools/scripts/decrypt_test.sh
+++ devtools/scripts/decrypt_test.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
-
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $HOME/secrets/test.txt test.txt.gpg
-
-echo $HOME/secrets/test.txt
+gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $TEST_FILE test.txt.gpg
