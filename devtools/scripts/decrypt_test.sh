a2c8f27249e34c208d8262191ca61c74508c8481
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-03 14:57:58 -0700

    Use absolute path.

--- devtools/scripts/decrypt_test.sh
+++ devtools/scripts/decrypt_test.sh
@@ -4,4 +4,6 @@
 mkdir $HOME/secrets
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $TEST_FILE test.txt.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $HOME/secrets/test.txt test.txt.gpg
+
+echo $HOME/secrets/test.txt
