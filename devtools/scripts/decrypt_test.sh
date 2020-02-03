29b9ea42cbfa61340ecee9cfa9ce2539ed69eed3
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-03 15:32:27 -0700

    Try GH syntax.

--- devtools/scripts/decrypt_test.sh
+++ devtools/scripts/decrypt_test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo $TEST_FILE
+
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$TEST_PASSPHRASE" --output $TEST_FILE test.txt.gpg
