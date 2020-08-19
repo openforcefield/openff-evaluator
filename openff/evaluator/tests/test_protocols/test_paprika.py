0f4addc20526229ec1fb72edf7f0d5e18283ff3f
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-08-19 11:20:16 +0100

    [PAPRIKA] Adds protocols for generating and applying APR restraints. (#264)

--- openff/evaluator/tests/test_protocols/test_paprika.py
+++ openff/evaluator/tests/test_protocols/test_paprika.py
@@ -11,6 +11,12 @@ from openff.evaluator.protocols.paprika.coordinates import (
     _atom_indices_by_role,
     _components_by_role,
 )
+from openff.evaluator.protocols.paprika.restraints import (
+    ApplyRestraints,
+    GenerateAttachRestraints,
+    GeneratePullRestraints,
+    GenerateReleaseRestraints,
+)
 from openff.evaluator.substances import Component, ExactAmount, Substance
 from openff.evaluator.utils import get_data_filename
 
@@ -28,6 +34,95 @@ def dummy_complex() -> Substance:
     )
 
     return substance
+
+
+@pytest.fixture()
+def complex_file_path(tmp_path):
+
+    import parmed.geometry
+    from paprika.setup import Setup
+
+    complex_path = get_data_filename(
+        os.path.join("test", "molecules", "methanol_methane.pdb")
+    )
+
+    # noinspection PyTypeChecker
+    structure: parmed.Structure = parmed.load_file(complex_path, structure=True)
+    # noinspection PyTypeChecker
+    center_of_mass = parmed.geometry.center_of_mass(
+        structure.coordinates, masses=numpy.ones(len(structure.coordinates))
+    )
+
+    Setup.add_dummy_atoms_to_structure(
+        structure,
+        [
+            numpy.array([0.0, 0.0, 10.0]),
+            numpy.array([0.0, 0.0, 20.0]),
+            numpy.array([0.0, 5.0, 25.0]),
+        ],
+        center_of_mass,
+    )
+
+    complex_path = os.path.join(tmp_path, "complex.pdb")
+    structure.save(complex_path)
+
+    return complex_path
+
+
+@pytest.fixture(scope="module")
+def restraints_schema():
+    return {
+        "static": [{"atoms": "@12 @1", "force_constant": 5.0}],
+        "conformational": [
+            {"atoms": "@1 @2 @3 @4", "force_constant": 6.0, "target": 104.3}
+        ],
+        "symmetry": [{"atoms": "@12 @7 @3 @4", "force_constant": 50.0, "target": 11.0}],
+        "wall": [{"atoms": "@12 @7 @3 @4", "force_constant": 50.0, "target": 11.0}],
+        "guest": [
+            {
+                "atoms": "@12 @7",
+                "attach": {"force_constant": 5.0, "target": 6.0},
+                "pull": {"force_constant": 5.0, "target": 24.0},
+            }
+        ],
+    }
+
+
+@pytest.fixture()
+def attach_restraints_path(tmp_path, complex_file_path, restraints_schema):
+
+    protocol = GenerateAttachRestraints("")
+    protocol.complex_coordinate_path = complex_file_path
+    protocol.attach_lambdas = [1.0]
+    protocol.restraint_schemas = restraints_schema
+    protocol.execute(str(tmp_path))
+
+    return protocol.restraints_path
+
+
+@pytest.fixture()
+def pull_restraints_path(tmp_path, complex_file_path, restraints_schema):
+
+    protocol = GeneratePullRestraints("")
+    protocol.complex_coordinate_path = complex_file_path
+    protocol.attach_lambdas = [0.0]
+    protocol.n_pull_windows = 2
+    protocol.restraint_schemas = restraints_schema
+    protocol.execute(str(tmp_path))
+
+    return protocol.restraints_path
+
+
+@pytest.fixture()
+def release_restraints_path(tmp_path, complex_file_path, restraints_schema):
+
+    protocol = GenerateReleaseRestraints("")
+    protocol.host_coordinate_path = complex_file_path
+    protocol.release_lambdas = [1.0]
+    protocol.restraint_schemas = restraints_schema
+    protocol.execute(str(tmp_path))
+
+    return protocol.restraints_path
 
 
 def test_components_by_role(dummy_complex):
@@ -165,3 +260,146 @@ def test_add_dummy_atoms(tmp_path, dummy_complex):
 
     assert system.getNumForces() == 1
     assert system.getForce(0).getNumParticles() == 3
+
+
+def validate_generated_restraints(restraints_path, expected_restraint_types, phase):
+
+    restraints_dictionary = ApplyRestraints.load_restraints(restraints_path)
+    restraints_dictionary = {
+        restraint_type: restraints
+        for restraint_type, restraints in restraints_dictionary.items()
+        if len(restraints) > 0
+    }
+
+    assert {*restraints_dictionary} == expected_restraint_types
+
+    restraints = [
+        restraint
+        for restraints in restraints_dictionary.values()
+        for restraint in restraints
+    ]
+
+    assert all(
+        "release" in restraint.phase and restraint.phase[phase] is not None
+        for restraint in restraints
+    )
+
+
+def validate_system_file(system_path, expected_force_groups):
+
+    from simtk import openmm
+
+    assert os.path.isfile(system_path)
+
+    with open(system_path) as file:
+        system: openmm.System = openmm.XmlSerializer.deserialize(file.read())
+
+    assert system.getNumForces() == len(expected_force_groups) + 3
+
+    for force in system.getForces():
+        assert force.getForceGroup() in {*expected_force_groups, 15}
+
+
+def test_generate_attach_restraints(tmp_path, complex_file_path, restraints_schema):
+
+    protocol = GenerateAttachRestraints("")
+    protocol.complex_coordinate_path = complex_file_path
+    protocol.attach_lambdas = [0.0, 0.5, 1.0]
+    protocol.restraint_schemas = restraints_schema
+
+    protocol.execute(str(tmp_path))
+
+    assert os.path.isfile(protocol.restraints_path)
+
+    validate_generated_restraints(
+        protocol.restraints_path,
+        {"static", "conformational", "guest", "wall", "symmetry"},
+        "attach",
+    )
+
+
+def test_generate_pull_restraints(tmp_path, complex_file_path, restraints_schema):
+
+    protocol = GeneratePullRestraints("")
+    protocol.complex_coordinate_path = complex_file_path
+    protocol.attach_lambdas = [0.0, 1.0]
+    protocol.n_pull_windows = 2
+    protocol.restraint_schemas = restraints_schema
+
+    protocol.execute(str(tmp_path))
+
+    assert os.path.isfile(protocol.restraints_path)
+
+    validate_generated_restraints(
+        protocol.restraints_path, {"static", "conformational", "guest"}, "pull"
+    )
+
+
+def test_generate_release_restraints(tmp_path, complex_file_path, restraints_schema):
+
+    protocol = GenerateReleaseRestraints("")
+    protocol.host_coordinate_path = complex_file_path
+    protocol.release_lambdas = [1.0, 0.0]
+    protocol.restraint_schemas = restraints_schema
+
+    protocol.execute(str(tmp_path))
+
+    assert os.path.isfile(protocol.restraints_path)
+
+    validate_generated_restraints(
+        protocol.restraints_path, {"static", "conformational"}, "release"
+    )
+
+
+def test_apply_attach_restraints(tmp_path, complex_file_path, attach_restraints_path):
+
+    from simtk import openmm
+
+    with open(os.path.join(tmp_path, "system.xml"), "w") as file:
+        file.write(openmm.XmlSerializer.serialize(openmm.System()))
+
+    protocol = ApplyRestraints("")
+    protocol.restraints_path = attach_restraints_path
+    protocol.input_coordinate_path = complex_file_path
+    protocol.input_system_path = os.path.join(tmp_path, "system.xml")
+    protocol.phase = "attach"
+    protocol.window_index = 0
+    protocol.execute(str(tmp_path))
+
+    validate_system_file(protocol.output_system_path, {10, 11, 12, 13, 14})
+
+
+def test_apply_pull_restraints(tmp_path, complex_file_path, pull_restraints_path):
+
+    from simtk import openmm
+
+    with open(os.path.join(tmp_path, "system.xml"), "w") as file:
+        file.write(openmm.XmlSerializer.serialize(openmm.System()))
+
+    protocol = ApplyRestraints("")
+    protocol.restraints_path = pull_restraints_path
+    protocol.input_coordinate_path = complex_file_path
+    protocol.input_system_path = os.path.join(tmp_path, "system.xml")
+    protocol.phase = "pull"
+    protocol.window_index = 0
+    protocol.execute(str(tmp_path))
+
+    validate_system_file(protocol.output_system_path, {10, 11, 12})
+
+
+def test_apply_release_restraints(tmp_path, complex_file_path, release_restraints_path):
+
+    from simtk import openmm
+
+    with open(os.path.join(tmp_path, "system.xml"), "w") as file:
+        file.write(openmm.XmlSerializer.serialize(openmm.System()))
+
+    protocol = ApplyRestraints("")
+    protocol.restraints_path = release_restraints_path
+    protocol.input_coordinate_path = complex_file_path
+    protocol.input_system_path = os.path.join(tmp_path, "system.xml")
+    protocol.phase = "release"
+    protocol.window_index = 0
+    protocol.execute(str(tmp_path))
+
+    validate_system_file(protocol.output_system_path, {10, 11})
