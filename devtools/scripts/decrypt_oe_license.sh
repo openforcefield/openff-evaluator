85ecdb73d431d4375e36263ccd68934e97fb1800
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-12-18 09:57:54 +0000

    Update the CI scripts to closely match other OpenFF repos.

--- devtools/scripts/decrypt_oe_license.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$OE_LICENSE_PASSPHRASE" --output $OE_LICENSE oe_license.txt.gpg
