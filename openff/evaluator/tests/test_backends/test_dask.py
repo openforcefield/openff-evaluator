998fd55c36798cea7f759f1ececc7c5a57d1539e
Pavan Behara <pavan.behara@openforcefield.org>
2021-08-17 08:21:34 -0700

    tutorials reverted back, test added to test_dask.py

--- openff/evaluator/tests/test_backends/test_dask.py
+++ openff/evaluator/tests/test_backends/test_dask.py
@@ -24,7 +24,7 @@ def test_dask_job_script_creation():
     cpu_backend.stop()
 
 
-@pytest.mark.parametrize("cluster_class", [DaskLSFBackend, DaskPBSBackend])
+@pytest.mark.parametrize("cluster_class", [DaskLSFBackend, DaskPBSBackend, DaskSLURMBackend])
 def test_dask_jobqueue_backend_creation(cluster_class):
     """Test creating and starting a new dask jobqueue backend."""
 
