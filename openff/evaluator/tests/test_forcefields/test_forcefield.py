456056af8d3def2c0b0b4f81d24cdf4ed06e242d
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-06-16 11:47:21 -0500

    Do not assume plugins not loaded

--- openff/evaluator/tests/test_forcefields/test_forcefield.py
+++ openff/evaluator/tests/test_forcefields/test_forcefield.py
@@ -1,5 +1,3 @@
-import pytest
-
 from openff.evaluator.forcefield import SmirnoffForceFieldSource
 from openff.evaluator.utils import get_data_filename
 
@@ -12,11 +10,3 @@ def test_load_smirnoff_plugins():
     obj = SmirnoffForceFieldSource.from_path(force_field_path, load_plugins=True)
 
     assert "DampedBuckingham68" in obj.to_force_field().registered_parameter_handlers
-
-    SmirnoffForceFieldSource.from_path(force_field_path)
-
-    with pytest.raises(
-        KeyError,
-        match="Cannot find a registered parameter handler class for tag 'DampedBuckingham68'",
-    ):
-        SmirnoffForceFieldSource.from_path(force_field_path)
