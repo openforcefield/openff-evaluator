56aa14fb6a2331a1f28978ea1b833b595751e773
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-02-09 13:54:59 -0600

    Remove protocols based on Yank

--- openff/evaluator/properties/__init__.py
+++ openff/evaluator/properties/__init__.py
@@ -1,15 +1,11 @@
-from .binding import HostGuestBindingAffinity
 from .density import Density, ExcessMolarVolume
 from .dielectric import DielectricConstant
 from .enthalpy import EnthalpyOfMixing, EnthalpyOfVaporization
-from .solvation import SolvationFreeEnergy
 
 __all__ = [
-    HostGuestBindingAffinity,
     Density,
     ExcessMolarVolume,
     DielectricConstant,
     EnthalpyOfMixing,
     EnthalpyOfVaporization,
-    SolvationFreeEnergy,
 ]
