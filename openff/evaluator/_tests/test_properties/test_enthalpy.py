5e53668e18f3b728c0eb8e0df2aec6d8f548fbe3
Lily Wang <lily@mdanalysis.org>
2024-10-01 21:51:21 +1000

    fix tests

--- openff/evaluator/_tests/test_properties/test_enthalpy.py
+++ openff/evaluator/_tests/test_properties/test_enthalpy.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import os
+=======
+import json
+>>>>>>> 223075d (fix tests)
 import pathlib
 import shutil
 
@@ -16,6 +20,11 @@ from openff.evaluator.properties import EnthalpyOfMixing
 from openff.evaluator.protocols import analysis
 from openff.evaluator.substances import Component, MoleFraction, Substance
 from openff.evaluator.thermodynamics import ThermodynamicState
+<<<<<<< HEAD
+=======
+from openff.evaluator.workflow import Workflow
+from openff.evaluator.utils.serialization import TypedJSONEncoder
+>>>>>>> 223075d (fix tests)
 from openff.evaluator.utils import graph
 from openff.evaluator.utils.observables import Observable
 from openff.evaluator.workflow import Workflow
@@ -73,16 +82,21 @@ class TestEnthalpyOfMixing:
         "input_mole_fractions, output_mole_fractions",
         [
             [(0.5, 0.5), (0.5, 0.5)],
+<<<<<<< HEAD
             [(0.1037, 0.8963), (0.104, 0.896)],
             [(0.1111, 0.2222, 0.1667), (0.222, 0.444, 0.334)],
         ],
+=======
+            [(0.1037, 0.8963), (0.10, 0.90)],
+        ]
+>>>>>>> 223075d (fix tests)
     )
     def test_mole_fractions(self, input_mole_fractions, output_mole_fractions, tmpdir):
         """
         This test *only* checks the part where mole fractions are weighted
         """
         # build our enthalpy of mixing target
-        default_schema = EnthalpyOfMixing.default_simulation_schema(n_molecules=1000)
+        default_schema = EnthalpyOfMixing.default_simulation_schema(n_molecules=100)
         workflow_schema = default_schema.workflow_schema
         workflow_schema.replace_protocol_types(
             {"BaseBuildSystem": "BuildSmirnoffSystem"}
@@ -117,8 +131,9 @@ class TestEnthalpyOfMixing:
             parent_outputs = []
             for name, protocol in workflow_graph.protocols.items():
                 if "build" in name:
+                    build_path = name.replace("|", "_")
                     output = protocol_graph._execute_protocol(
-                        ".",
+                        build_path,
                         protocol,
                         True,
                         available_resources=None,
@@ -129,22 +144,38 @@ class TestEnthalpyOfMixing:
 
             for i in range(n_components):
                 # generate fake data for the conditional groups
+                base = f"extract_observable_component_{i}"
                 path = (
                     here
                     / f"{uuid}_conditional_group_component_{i}"
-                    / f"{uuid}_extract_observable_component_{i}"
-                    / f"{uuid}|extract_observable_component_{i}.json"
+                    / f"{uuid}_{base}"
+                    / f"{uuid}|{base}.json"
                 )
                 path.parent.mkdir(parents=True, exist_ok=True)
                 name = f"extract_observable_component_{i}"
+                observable = _generate_dummy_observable(name)
                 with path.open("w") as file:
-                    file.write(_generate_dummy_observable(name).json())
+                    file.write(
+                        json.dumps(
+                            {
+                                ".value": observable.value,
+                                ".time_series_statistics": observable.time_series_statistics,
+                            },
+                            cls=TypedJSONEncoder,
+                        )
+                    )
+                parent_outputs.append((f"{uuid}|{base}", str(path)))
 
                 # now check mole fraction protocol execution
                 cg = workflow_graph.protocols[f"{uuid}|conditional_group_component_{i}"]
                 wmf = cg.protocols[f"{uuid}|weight_by_mole_fraction_{i}"]
+                wmf_path = (
+                    here
+                    / f"{uuid}_conditional_group_component_{i}"
+                    / f"{uuid}_weight_by_mole_fraction_{i}"
+                )
                 protocol_graph._execute_protocol(
-                    ".",
+                    str(wmf_path),
                     wmf,
                     True,
                     *parent_outputs,
@@ -164,6 +195,11 @@ class TestEnthalpyOfMixing:
                     output_mole_fractions[i] * 10,
                 )
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 223075d (fix tests)
     def test_expected_output_from_production_simulation(self, tmpdir):
         """
         This is an integration test of sorts,
@@ -236,84 +272,84 @@ class TestEnthalpyOfMixing:
 
             result = workflow.execute()
 
-        # manually check mole fractions
-        substance0 = workflow.protocols[
-            f"{uuid}|build_coordinates_component_0"
-        ].output_substance
-        substance1 = workflow.protocols[
-            f"{uuid}|build_coordinates_component_1"
-        ].output_substance
-        substance_mix = workflow.protocols[
-            f"{uuid}|build_coordinates_mixture"
-        ].output_substance
+            # manually check mole fractions
+            substance0 = workflow.protocols[
+                f"{uuid}|build_coordinates_component_0"
+            ].output_substance
+            substance1 = workflow.protocols[
+                f"{uuid}|build_coordinates_component_1"
+            ].output_substance
+            substance_mix = workflow.protocols[
+                f"{uuid}|build_coordinates_mixture"
+            ].output_substance
 
-        assert len(substance0.amounts) == 1
-        assert len(substance1.amounts) == 1
-        assert len(substance_mix.amounts) == 2
+            assert len(substance0.amounts) == 1
+            assert len(substance1.amounts) == 1
+            assert len(substance_mix.amounts) == 2
 
-        assert np.isclose(substance0.amounts[r"O{solv}"][0].value, 1)
-        assert np.isclose(substance1.amounts[r"OCCN(CCO)CCO{solv}"][0].value, 1)
-        assert np.isclose(substance_mix.amounts[r"O{solv}"][0].value, 0.51)
-        assert np.isclose(substance_mix.amounts[r"OCCN(CCO)CCO{solv}"][0].value, 0.49)
+            assert np.isclose(substance0.amounts[r"O{solv}"][0].value, 1)
+            assert np.isclose(substance1.amounts[r"OCCN(CCO)CCO{solv}"][0].value, 1)
+            assert np.isclose(substance_mix.amounts[r"O{solv}"][0].value, 0.51)
+            assert np.isclose(substance_mix.amounts[r"OCCN(CCO)CCO{solv}"][0].value, 0.49)
 
-        # check assignment and parameterization
-        system0 = workflow.protocols[
-            f"{uuid}|assign_parameters_component_0"
-        ].parameterized_system
-        system1 = workflow.protocols[
-            f"{uuid}|assign_parameters_component_1"
-        ].parameterized_system
-        system_mix = workflow.protocols[
-            f"{uuid}|assign_parameters_mixture"
-        ].parameterized_system
+            # check assignment and parameterization
+            system0 = workflow.protocols[
+                f"{uuid}|assign_parameters_component_0"
+            ].parameterized_system
+            system1 = workflow.protocols[
+                f"{uuid}|assign_parameters_component_1"
+            ].parameterized_system
+            system_mix = workflow.protocols[
+                f"{uuid}|assign_parameters_mixture"
+            ].parameterized_system
 
-        for system in [system0, system1, system_mix]:
-            assert isinstance(system.topology, Topology)
-            assert system.topology.n_molecules == 1000
-            assert isinstance(system.system, OpenMMSystem)
+            for system in [system0, system1, system_mix]:
+                assert isinstance(system.topology, Topology)
+                assert system.topology.n_molecules == 1000
+                assert isinstance(system.system, OpenMMSystem)
 
-        # check cg:= conditional_group. This is a group of protocols
-        cg0 = workflow.protocols[f"{uuid}|conditional_group_component_0"]
-        cg1 = workflow.protocols[f"{uuid}|conditional_group_component_1"]
-        cg_mix = workflow.protocols[f"{uuid}|conditional_group_mixture"]
+            # check cg:= conditional_group. This is a group of protocols
+            cg0 = workflow.protocols[f"{uuid}|conditional_group_component_0"]
+            cg1 = workflow.protocols[f"{uuid}|conditional_group_component_1"]
+            cg_mix = workflow.protocols[f"{uuid}|conditional_group_mixture"]
 
-        # check enthalpy is correct
-        # note this is not re-simulated, but it *is* re-calculated
-        enth0 = cg0.protocols[f"{uuid}|extract_observable_component_0"].value
-        enth1 = cg1.protocols[f"{uuid}|extract_observable_component_1"].value
-        enth_mix = cg_mix.protocols[f"{uuid}|extract_observable_mixture"].value
+            # check enthalpy is correct
+            # note this is not re-simulated, but it *is* re-calculated
+            enth0 = cg0.protocols[f"{uuid}|extract_observable_component_0"].value
+            enth1 = cg1.protocols[f"{uuid}|extract_observable_component_1"].value
+            enth_mix = cg_mix.protocols[f"{uuid}|extract_observable_mixture"].value
 
-        assert np.isclose(
-            enth0.value.m_as(unit.kilojoules_per_mole), -44.879, atol=1e-3
-        )
-        assert np.isclose(
-            enth1.value.m_as(unit.kilojoules_per_mole), 251.418, atol=1e-3
-        )
-        assert np.isclose(
-            enth_mix.value.m_as(unit.kilojoules_per_mole), 98.855, atol=1e-3
-        )
+            assert np.isclose(enth0.value.m_as(unit.kilojoules_per_mole), -44.879, atol=1e-3)
+            assert np.isclose(enth1.value.m_as(unit.kilojoules_per_mole), 251.418, atol=1e-3)
+            assert np.isclose(
+                enth_mix.value.m_as(unit.kilojoules_per_mole), 98.855, atol=1e-3
+            )
 
-        # now check weighting by mole fraction
-        wmf0 = cg0.protocols[f"{uuid}|weight_by_mole_fraction_0"]
-        wmf1 = cg1.protocols[f"{uuid}|weight_by_mole_fraction_1"]
+            # now check weighting by mole fraction
+            wmf0 = cg0.protocols[f"{uuid}|weight_by_mole_fraction_0"]
+            wmf1 = cg1.protocols[f"{uuid}|weight_by_mole_fraction_1"]
 
-        assert np.isclose(wmf0.full_substance.amounts[r"O{solv}"][0].value, 0.51)
-        assert np.isclose(
-            wmf1.full_substance.amounts[r"OCCN(CCO)CCO{solv}"][0].value, 0.49
-        )
+            assert np.isclose(
+                wmf0.full_substance.amounts[r"O{solv}"][0].value, 0.51
+            )
+            assert np.isclose(
+                wmf1.full_substance.amounts[r"OCCN(CCO)CCO{solv}"][0].value, 0.49
+            )
 
-        assert np.isclose(
-            wmf0.weighted_value.value.m_as(unit.kilojoules_per_mole), -22.888, atol=1e-3
-        )
-        assert np.isclose(
-            wmf1.weighted_value.value.m_as(unit.kilojoules_per_mole), 123.195, atol=1e-3
-        )
+            assert np.isclose(
+                wmf0.weighted_value.value.m_as(unit.kilojoules_per_mole), -22.888, atol=1e-3
+            )
+            assert np.isclose(
+                wmf1.weighted_value.value.m_as(unit.kilojoules_per_mole), 123.195, atol=1e-3
+            )
 
-        # check adding the two together
-        add = workflow.protocols[f"{uuid}|add_component_observables"].result
-        assert np.isclose(add.value.m_as(unit.kilojoules_per_mole), 100.307, atol=1e-3)
+            # check adding the two together
+            add = workflow.protocols[f"{uuid}|add_component_observables"].result
+            assert np.isclose(
+                add.value.m_as(unit.kilojoules_per_mole), 100.307, atol=1e-3
+            )
 
-        # check final value
-        assert np.isclose(
-            result.value.value.m_as(unit.kilojoules_per_mole), -1.452, atol=1e-3
-        )
+            # check final value
+            assert np.isclose(
+                result.value.value.m_as(unit.kilojoules_per_mole), -1.452, atol=1e-3
+            )
