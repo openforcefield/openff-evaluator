c8658d89c3e6b116b60ff0b85228cbea3f24594d
Matt Thompson <mattwthompson@protonmail.com>
2024-10-02 08:15:00 -0500

    Use new Pydantic v1 backdoor (#574)
    * Use new Pydantic v1 backdoor
    
    * Empty commit
    
    * Update release history

--- openff/evaluator/_pydantic.py
+++ openff/evaluator/_pydantic.py
@@ -1,32 +1,14 @@
-try:
-    from pydantic.v1 import (
-        BaseModel,
-        Field,
-        HttpUrl,
-        PositiveFloat,
-        PositiveInt,
-        ValidationError,
-        confloat,
-        conint,
-        conlist,
-        constr,
-        root_validator,
-        validator,
-    )
-    from pydantic.v1.validators import dict_validator
-except ModuleNotFoundError:
-    from pydantic import (
-        BaseModel,
-        Field,
-        HttpUrl,
-        PositiveFloat,
-        PositiveInt,
-        ValidationError,
-        confloat,
-        conint,
-        conlist,
-        constr,
-        root_validator,
-        validator,
-    )
-    from pydantic.validators import dict_validator
+from pydantic.v1 import (
+    BaseModel,
+    Field,
+    HttpUrl,
+    PositiveFloat,
+    PositiveInt,
+    ValidationError,
+    confloat,
+    conint,
+    conlist,
+    constr,
+    root_validator,
+    validator,
+)
