42256d4ff49f23892881b55fbb85c346aa86bef7
Matt Thompson <mattwthompson@protonmail.com>
2024-10-02 09:58:30 -0500

    Run Integration tests on AWS runners (#565)
    * try making AWS hello world yaml
    
    * update aws action to trigger on pull requests
    
    * fix type in yaml and get all these other jobs out of the way
    
    * cosmetic commit to kick ci
    
    * Run integration tests
    
    * Try fixing default path
    
    * Remove CI workflow while working on GPU action
    
    * Fix path typo
    
    * Install package locally
    
    * Remove YANK-based protocols from integration tests
    
    * Revert "Updates for `pint >=0.21` (#562)"
    
    This reverts commit c8c476ab6ec1e379049384ce23982e547d699f4b.
    
    * Skip CI
    
    * Bump runner version
    
    * Use "local" Dask cluster
    
    https://docs.openforcefield.org/projects/evaluator/en/stable/backends/daskbackends.html#dask-local-cluster
    
    * Set CUDA device
    
    * Fix
    
    * Add CI back
    
    * Undo obsolete Pint changes
    
    * Fix micromamba version
    
    * Bump runner stopper
    
    ---------
    
    Co-authored-by: Jeff Wagner <jwagnerjpl@gmail.com>

--- integration-tests/default-workflows/run.py
+++ integration-tests/default-workflows/run.py
@@ -4,27 +4,24 @@ from openff.toolkit.typing.engines.smirnoff import ForceField
 from openff.units import unit
 
 from openff import evaluator
-from openff.evaluator.backends import QueueWorkerResources
-from openff.evaluator.backends.dask import DaskLSFBackend
+from openff.evaluator.backends.backends import ComputeResources
+from openff.evaluator.backends.dask import DaskLocalCluster
 from openff.evaluator.client import EvaluatorClient, RequestOptions
 from openff.evaluator.datasets import PhysicalPropertyDataSet, PropertyPhase
 from openff.evaluator.datasets.curation.components.filtering import (
     FilterBySubstancesSchema,
 )
-from openff.evaluator.datasets.curation.components.freesolv import ImportFreeSolvSchema
 from openff.evaluator.datasets.curation.workflow import (
     CurationWorkflow,
     CurationWorkflowSchema,
 )
 from openff.evaluator.forcefield import ParameterGradientKey
-from openff.evaluator.layers.simulation import SimulationSchema
 from openff.evaluator.properties import (
     Density,
     DielectricConstant,
     EnthalpyOfMixing,
     EnthalpyOfVaporization,
     ExcessMolarVolume,
-    SolvationFreeEnergy,
 )
 from openff.evaluator.server import EvaluatorServer
 from openff.evaluator.storage import LocalFileStorage
@@ -32,6 +29,8 @@ from openff.evaluator.substances import Component, ExactAmount, Substance
 from openff.evaluator.thermodynamics import ThermodynamicState
 from openff.evaluator.utils import setup_timestamp_logging
 from openff.evaluator.utils.utils import temporarily_change_directory
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 def define_data_set(reweighting: bool) -> PhysicalPropertyDataSet:
@@ -62,23 +61,10 @@ def define_data_set(reweighting: bool) -> PhysicalPropertyDataSet:
         )
 
         data_set.add_properties(
-            SolvationFreeEnergy(
-                thermodynamic_state=states[1],
-                phase=PropertyPhase.Liquid,
-                substance=ethanol_substance,
-                value=0.0 * SolvationFreeEnergy.default_unit(),
-            ),
-            SolvationFreeEnergy(
-                thermodynamic_state=states[1],
-                phase=PropertyPhase.Liquid,
-                substance=ethanal_substance,
-                value=0.0 * SolvationFreeEnergy.default_unit(),
-            ),
             *CurationWorkflow.apply(
                 PhysicalPropertyDataSet(),
                 CurationWorkflowSchema(
                     component_schemas=[
-                        ImportFreeSolvSchema(),
                         FilterBySubstancesSchema(substances_to_include=[("O", "CO")]),
                     ]
                 ),
@@ -126,75 +112,6 @@ def define_data_set(reweighting: bool) -> PhysicalPropertyDataSet:
     return data_set
 
 
-def solvation_free_energy_schema() -> SimulationSchema:
-    """Override trailblazing to use the lambda values of used in the previous OFF study
-    https://github.com/MobleyLab/SMIRNOFF_paper_code/tree/master/FreeSolv
-    """
-
-    default_schema = SolvationFreeEnergy.default_simulation_schema()
-    workflow_schema = default_schema.workflow_schema
-
-    conditional_group_schema = next(
-        x for x in workflow_schema.protocol_schemas if x.id == "conditional_group"
-    )
-    conditional_group = conditional_group_schema.to_protocol()
-
-    yank_protocol = conditional_group.protocols["run_solvation_yank"]
-
-    yank_protocol.electrostatic_lambdas_1 = [
-        1.00,
-        0.75,
-        0.50,
-        0.25,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-        0.00,
-    ]
-    yank_protocol.steric_lambdas_1 = [
-        1.00,
-        1.00,
-        1.00,
-        1.00,
-        1.00,
-        0.95,
-        0.90,
-        0.80,
-        0.70,
-        0.60,
-        0.50,
-        0.40,
-        0.35,
-        0.30,
-        0.25,
-        0.20,
-        0.15,
-        0.10,
-        0.05,
-        0.00,
-    ]
-
-    yank_protocol.electrostatic_lambdas_2 = [1.00, 0.75, 0.50, 0.25, 0.00]
-    yank_protocol.steric_lambdas_2 = [1.00, 1.00, 1.00, 1.00, 1.00]
-
-    workflow_schema.protocol_schemas.remove(conditional_group_schema)
-    workflow_schema.protocol_schemas.append(conditional_group.schema)
-
-    return default_schema
-
-
 def main():
     setup_timestamp_logging()
 
@@ -208,29 +125,23 @@ def main():
     os.makedirs(os.path.join(version, "results"))
 
     with temporarily_change_directory(version):
-        with DaskLSFBackend(
-            minimum_number_of_workers=1,
-            maximum_number_of_workers=12,
-            resources_per_worker=QueueWorkerResources(
+        with DaskLocalCluster(
+            number_of_workers=1,
+            resources_per_worker=ComputeResources(
+                number_of_threads=1,
                 number_of_gpus=1,
-                preferred_gpu_toolkit=QueueWorkerResources.GPUToolkit.CUDA,
-                per_thread_memory_limit=5 * unit.gigabyte,
-                wallclock_time_limit="05:59",
+                preferred_gpu_toolkit=ComputeResources.GPUToolkit.CUDA,
             ),
-            setup_script_commands=[
-                f"conda activate openff-evaluator-{version}",
-                "module load cuda/10.0",
-            ],
-            queue_name="gpuqueue",
-        ) as calculation_backend:
+        ) as local_backend:
+
             with EvaluatorServer(
-                calculation_backend,
+                local_backend,
                 working_directory="outputs",
                 storage_backend=LocalFileStorage("cached-data"),
             ):
                 client = EvaluatorClient()
 
-                for allowed_layer in ["SimulationLayer", "ReweightingLayer"]:
+                for allowed_layer in ["ReweightingLayer"]:
                     data_set = define_data_set(allowed_layer == "ReweightingLayer")
 
                     options = RequestOptions()
@@ -238,13 +149,6 @@ def main():
                     options.calculation_schemas = {
                         property_type: {} for property_type in data_set.property_types
                     }
-
-                    if allowed_layer == "SimulationLayer":
-                        options.add_schema(
-                            "SimulationLayer",
-                            "SolvationFreeEnergy",
-                            solvation_free_energy_schema(),
-                        )
 
                     request, _ = client.request_estimate(
                         data_set,
