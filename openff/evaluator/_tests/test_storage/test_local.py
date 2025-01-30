02e6c3c472f25ae9b45615c2f229ce9701895ea0
Matt Thompson <matt.thompson@openforcefield.org>
2025-01-30 17:44:29 -0600

    Migrate `tmpdir` fixture to new `tmp_path` (#615)

--- openff/evaluator/_tests/test_storage/test_local.py
+++ openff/evaluator/_tests/test_storage/test_local.py
@@ -7,8 +7,8 @@ import os
 from openff.evaluator.storage import LocalFileStorage
 
 
-def test_root_directory(tmpdir):
-    local_storage_path = os.path.join(tmpdir, "stored-data")
+def test_root_directory(tmp_path):
+    local_storage_path = os.path.join(tmp_path, "stored-data")
     local_storage = LocalFileStorage(root_directory=local_storage_path)
 
     assert os.path.isdir(local_storage_path)
