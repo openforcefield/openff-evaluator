5b3c45d97cc300dacef7e15e9abc08b6d974ffc9
Matt Thompson <matt.thompson@openforcefield.org>
2025-05-30 11:02:36 -0500

    Drop `importlib.metadata` backport (#663)

--- openff/evaluator/datasets/taproom/taproom.py
+++ openff/evaluator/datasets/taproom/taproom.py
@@ -432,13 +432,7 @@ class TaproomDataSet(PhysicalPropertyDataSet):
             Whether to add the metadata required for an APR based calculation
             using the ``paprika`` based workflow.
         """
-        import sys
-
-        if sys.version_info[1] < 10:
-            # Backport only for Python 3.9 - drop April 2024
-            from importlib_metadata import entry_points
-        else:
-            from importlib.metadata import entry_points
+        from importlib.metadata import entry_points
 
         installed_benchmarks = {}
 
