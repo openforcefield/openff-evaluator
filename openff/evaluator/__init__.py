d1ad8c4b8bff593351436f9e7cdadb3b4ddce326
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-10-02 10:06:14 -0500

    Add smirnoff99Frosst dependency
    Update project configuration
    
    Switch to `setuptools_scm`
    
    Fix version handling
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Fix

--- openff/evaluator/__init__.py
+++ openff/evaluator/__init__.py
@@ -3,18 +3,17 @@ openff-evaluator
 A physical property evaluation toolkit from the Open Forcefield Consortium.
 """
 
+from importlib.metadata import version
+
 from openff.units import unit
 
-from ._version import get_versions
-from .plugins import register_default_plugins, register_external_plugins
+from openff.evaluator.plugins import register_default_plugins, register_external_plugins
 
 # Load the default plugins
 register_default_plugins()
 # Load in any found external plugins.
 register_external_plugins()
 
-# Handle versioneer
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+__version__ = version("openff.evaluator")
+
+__all__ = ("unit",)
